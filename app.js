03bcc33ed241b65bce9725e01e233374999b3488
NehaShende27 <113248053+NehaShende27@users.noreply.github.com>
2024-02-05 18:00:47 -0500

    added user creation logic (#2)
    * added models, routes, controllers
    
    * added user creation endpoint

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const healthRoutes = require('./routes/allRoutes.js');
+const db = require ('./models/databaseModel.js')
+
 
 const app = express();
 const PORT = 8080;
@@ -8,10 +10,18 @@ app.use(express.json());
 
 
 // Routes
-app.use('/', healthRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+db.connect()
+  .then(()=>{
+    app.use('/', healthRoutes);
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+.catch(error =>{
+  console.error('error while connecting to db',error);
+
 });
 
 module.exports= app;
--- routes/allRoutes.js
+++ routes/allRoutes.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const healthController = require('../controllers/healthController.js');
+const userController = require('../controllers/userController.js')
 
 const router = express.Router();
 
 
 router.all('/healthz', healthController.healthCheck);
+
+router.post('/v1/user', userController.createUser);
 
 module.exports = router;
