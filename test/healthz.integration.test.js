4fc35ae8875854e3b65b94194cf26fc20a3e3389
NehaShende27 <113248053+NehaShende27@users.noreply.github.com>
2024-02-07 18:01:47 -0500

    added integration tests, modified put method

--- test/healthz.integration.test.js
+++ test/healthz.integration.test.js
@@ -1,16 +1,15 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const expect = chai.expect;
 const app = require('../app.js');
 
 chai.use(chaiHttp);
-
+const {expect} = chai;
 describe('/healthz endpoint', () => {
   it('should return status 200 and "OK" for GET request', async() => {
     const res = await chai.request(app).get('/healthz');
     expect(res).to.have.status(200);
     expect(res.text).to.equal('OK');
-
+    
     after(function () {
       process.exit(0); // Use 0 for success, or any other value for failure
     });
