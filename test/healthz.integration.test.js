9d7c70211266b1b0dd3846546ed311b192871614
NehaShende27 <113248053+NehaShende27@users.noreply.github.com>
2024-02-07 11:27:18 -0500

    added workflow dispatch

---
+++ test/healthz.integration.test.js
@@ -0,0 +1,20 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const expect = chai.expect;
+const app = require('../app.js');
+
+chai.use(chaiHttp);
+
+describe('/healthz endpoint', () => {
+  it('should return status 200 and "OK" for GET request', async() => {
+    const res = await chai.request(app).get('/healthz');
+    expect(res).to.have.status(200);
+    expect(res.text).to.equal('OK');
+
+    after(function () {
+      process.exit(0); // Use 0 for success, or any other value for failure
+    });
+  });
+
+
+});
