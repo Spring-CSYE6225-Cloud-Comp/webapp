bd448b3ffcbf93bd54a3fe8f33cb07ad265c1404
NehaShende27 <113248053+NehaShende27@users.noreply.github.com>
2024-02-15 12:05:49 -0500

    Update healthz.integration.test.js

--- test/integration/healthz.integration.test.js
+++ test/integration/healthz.integration.test.js
@@ -52,8 +52,8 @@ describe('Account creation integration test',()=>{
     expect(getAccount).to.have.status(200);
     expect(getAccount.body).to.have.property('id');
     expect(getAccount.body.email).to.equal(reqBody.email);
-
-    after(function () {
+    done();
+    after(function (done) {
       process.exit(0); // Use 0 for success, or any other value for failure
     });
    
@@ -99,7 +99,8 @@ describe('Account updation integration test',()=>{
     expect(getAccount.body).to.have.property('id');
     expect(getAccount.body.email).to.equal(info.email);
 
-    after(function () {
+    done();
+    after(function (done) {
       process.exit(0); // Use 0 for success, or any other value for failure
     });
   })
