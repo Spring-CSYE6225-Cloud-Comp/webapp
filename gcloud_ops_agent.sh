37977d4013799b6ef3979a5b11715a4ab8497516
NehaShende27 <113248053+NehaShende27@users.noreply.github.com>
2024-03-20 17:35:23 -0400

    modified shell script (#91)
    * Update README.md
    
    * Update README.md
    
    * modified shell script

--- gcloud_ops_agent.sh
+++ gcloud_ops_agent.sh
@@ -12,24 +12,12 @@ sudo bash add-google-cloud-ops-agent-repo.sh --also-install
 # Configure Ops Agent for application logs
 #!/bin/bash
 
-cat << EOF | sudo tee /etc/google-cloud-ops-agent/config.yaml
-logging:
-  receivers:
-    my-app-receiver:
-      type: files
-      include_paths:
-        - /opt/csye6225/Neha_Shende_002783740_05/var/log/*.log
-      record_log_file_path: true
-  processors:
-    my-app-processor:
-      type: parse_json
-      time_key: time
-      time_format: "%Y-%m-%dT%H:%M:%S.%L%Z"
-  service:
-    pipelines:
-      default_pipeline:
-        receivers: [my-app-receiver]
-        processors: [my-app-processor]
-EOF
+# cat << EOF | sudo tee /etc/google-cloud-ops-agent/config.yaml
+
+# EOF
+
+# Move config.yaml to the specified path
+sudo mv config.yaml /etc/google-cloud-ops-agent/config.yaml
+
 
 sudo systemctl restart google-cloud-ops-agent
