2ef6410541a9f9acc08842eee3df1198dc343635
NehaShende27 <113248053+NehaShende27@users.noreply.github.com>
2024-04-15 14:25:12 -0400

    Update allRoutes.js

--- routes/allRoutes.js
+++ routes/allRoutes.js
@@ -6,9 +6,9 @@ const router = express.Router();
 
 router.all('/healthz', healthController.healthCheck);
 
-router.post('/v1/user', userController.createUser);
-router.get('/v1/user/self', userController.getUserInfo);
-router.put('/v1/user/self', userController.updateUser);
-router.get('/v1/user/verify',userController.verifyToken);
+router.post('/v2/user', userController.createUser);
+router.get('/v2/user/self', userController.getUserInfo);
+router.put('/v2/user/self', userController.updateUser);
+router.get('/v2/user/verify',userController.verifyToken);
 
 module.exports = router;
